2b863a8de445298bddb660ee24914924edbc4575
deeshashah <deeshashah29@gmail.com>
2019-01-26 16:48:44 -0500

    implemented the course service

---
+++ src/services/CourseService.js
@@ -0,0 +1,45 @@
+import courses from './courses.json'
+
+class CourseService{
+	constructor(){
+		this.courses = courses;
+	}
+
+	createCourse = course => {
+		if(course == null){
+			course = {
+				id : (new Date()).getTime(),
+				title: 'New Course'
+			}
+		}
+
+		this.courses.push(course)
+		return this.courses
+	}
+
+	findCourseById = courseId => {
+		this.courses = this.courses.find(
+				course => course.id === courseId
+			)
+	}
+
+	findAllCourses = () => {
+		this.courses
+	}
+
+	deleteCourse = deleteCourse => {
+		this.courses = this.courses.filter(
+				course => course.id !== deleteCourse.id
+			)
+	}
+
+	updateCourse = (id, course) => {
+		for(var i=0;i<this.courses.length;i++){
+			if(this.courses[i] == id){
+				this.courses[i] = course
+			}
+		}
+	}
+}
+
+export default CourseService
